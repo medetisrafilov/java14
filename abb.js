81863f69ae16f4a8b1250724ca23cec78cef1356
medetisrafilov <israfilovmedet52@gmail.com>
2024-12-16 19:07:23 -1200

    homework

---
+++ abb.js
@@ -0,0 +1,8 @@
+let students = ["Medet israfilov", "Kamran Memmedov", "Nur seferov"];
+
+
+function addStudent(fullName) {
+    students.push(fullName); 
+    console.log("Təzə siyahı:", students);
+}
+
